9f62ca76eaefe4d5cf6466b1af51437ae68e2622
Glushenko.Vitaliy <vts.glushenko@gmail.com>
2018-06-06 11:03:31 +0300

    search works

--- JS/index.js
+++ JS/index.js
@@ -1,131 +1,210 @@
 "use strict";
 
 let cars = [
-        {generation: `    <div class="fst">FiRST GENERATiON<div class="first">
-        <span class="s-class">
-            <img class="picture" src="IMG/S%20CLASS/1-st%20generation_1_%20for_cover.jpg">
-            <div class="bodywork">W140</div>
-        </span>
+    {
+        name: '',
+        img: '',
+        className: '',
+        generation: ''
+    },
+    {
+        name: 'W222',
+        img: 'IMG/S%20CLASS/4-st%20generation_1.jpg',
+        className: 'S CLASS',
+        generation: 'FOURTH GENERATION'
+    },
+    {
+        name: 'W213',
+        img: 'IMG/E CLASS/4-rd generation_1.jpg',
+        className: 'E CLASS',
+        generation: 'FOURTH GENERATION'
+    },
+    {
+        name: 'W205',
+        img: 'IMG/C CLASS/4-rd generation_1.jpg',
+        className: 'C CLASS',
+        generation: 'FOURTH GENERATION'
+    },
+    {
+        name: 'W221',
+        img: 'IMG/S%20CLASS/3-st%20generation_1.jpg',
+        className: 'S CLASS',
+        generation: 'THIRD GENERATION'
 
-        <span class="e-class">
-            <img class="picture" src="IMG/E%20CLASS/1-st%20generation_1.jpg">
-            <div class="bodywork">W124</div>
-        </span>
+    },
+    {
+        name: 'W212',
+        img: 'IMG/E CLASS/3-th generation_1.jpg',
+        className: 'E CLASS',
+        generation: 'THIRD GENERATION'
 
-        <span class="c-class">
-            <img class="picture" src="IMG/C%20CLASS/1-st%20generation_1.jpg">
-            <div class="bodywork">W210</div>
-        </span></div>
-    </div>`},
-    {generation: `    <div class="snd">SECOND GENERATiON<div class="second">
-        <span class="s-class">
-            <img class="picture" src="IMG/S%20CLASS/2-st%20generation_1.jpg">
-        </span>
+    },
+    {
+        name: 'W204',
+        img: 'IMG/C CLASS/3-th generation_1.jpg',
+        className: 'C CLASS',
+        generation: 'THIRD GENERATION'
+    },
+    {
+        name: 'W220',
+        img: 'IMG/S%20CLASS/2-st%20generation_1.jpg',
+        className: 'S CLASS',
+        generation: 'SECOND GENERATION'
 
-        <span class="e-class">
-            <img class="picture" src="IMG/e-class.jpg">
-        </span>
+    },
+    {
+        name: 'W211',
+        img: 'IMG/E%20CLASS/2-nd generation_1.jpg',
+        className: 'E CLASS',
+        generation: 'SECOND GENERATION'
 
-        <span class="c-class">
-            <img class="picture" src="IMG/c-class.jpg">
-        </span></div>
-    </div>`},
-    {generation: `    <div class="trd">THiRD GENERATiON<div class="third">
-        <span class="s-class">
-            <img class="picture" src="IMG/S%20CLASS/3-st%20generation_1.jpg">
-        </span>
+    },
+    {
+        name: 'W203',
+        img: 'IMG/C ClASS/2-nd generation_1.jpeg',
+        className: 'C CLASS',
+        generation: 'SECOND GENERATION'
+    },
+    {
+        name: 'W140',
+        img: 'IMG/S%20CLASS/1-st%20generation_1_%20for_cover.jpg',
+        className: 'S CLASS',
+        generation: 'FIRST GENERATION',
+        title: '',
+        information: '',
+        images: {}
 
-        <span class="e-class">
-            <img class="picture" src="IMG/e-class.jpg">
-        </span>
+    },
+    {
+        name: 'W210',
+        img: 'IMG/E%20CLASS/1-st generation_1.jpg',
+        className: 'E CLASS',
+        generation: 'FIRST GENERATION'
 
-        <span class="c-class">
-            <img class="picture" src="IMG/c-class.jpg">
-        </span></div>
-    </div>`},
-    {generation: `    <div class="fth">FOURTH GENERATiON<div class="fourth">
-        <span class="s-class">
-            <img class="picture" src="IMG/S%20CLASS/4-st%20generation_1.jpg">
-        </span>
+    },
+    {
+        name: 'W202',
+        img: 'IMG/C CLASS/1-st generation_1.jpg',
+        className: 'C CLASS',
+        generation: 'FIRST GENERATION'
+    }
 
-        <span class="e-class">
-            <img class="picture" src="IMG/e-class.jpg">
-        </span>
-
-        <span class="c-class">
-            <img class="picture" src="IMG/c-class.jpg">
-        </span></div>
-    </div>`}
 ];
 
-// let container = document.getElementById('all_generations');
-//
-// function initCarList() {
-//     let listResult = '';
-//     for(let i = 0; i < cars.length; i++) {
-//         let car = cars[i];
-//         listResult += getCarItemHtml(car);
-//     }
-//     container.innerHTML = listResult;
-// }
-//
-// function getCarItemHtml(car) {
-//     return car.generation;
-// }
-//
-// initCarList();
+let generationsArray = [];
 
 
+let container = document.getElementById('all_generations');
+let searchEl = document.getElementById('search');
+
+searchEl.addEventListener('input', function (event) {
+    let value = event.target.value.toUpperCase();
+    if (value === '') {
+        generationsArray = [];
+        initCarList(cars);
+        return;
+    }
+
+    let filteredCars = cars.filter(function (carItem) {
+        if (carItem.className.indexOf(value) > -1 ) {
+            return true;
+        }
+        if (carItem.name.indexOf(value) > -1 ) {
+            return true;
+        }
+        if (carItem.generation.indexOf(value) > -1 ) {
+            return true;
+        }
+        else {
+             return false;
+        }
+
+    });
+
+    function searchResult(models) {
+        let modelsView = '';
+        container.style.flexFlow = 'row wrap';
+        for (let i = 0; i < models.length; i++) {
+            let model = models[i];
+            modelsView += modelView(model);
+            container.innerHTML = modelsView;
 
 
+        }
+
+    }
+    searchResult(filteredCars);
+
+});
+
+//container.addEventListener('click', function modelView(event));
+
+function initCarList(carsArray) {
+    container.innerHTML = generationView(carsArray);
+}
 
 
-// function SearchText() {
-//     let ResultSearch = '';
-//     for (let i = 0; i < cars.length; i++) {
-//         var list = cars[i];
-//         for (let j = InputSearch[i];)}
-//
-// }
-//
-// let InputSearch = document.querySelector('place_for_search');
+function generationView(models) {
+    let modelsView = '';
 
+    for (let i = 1; i < models.length; i++) {
+        let model = models[i];
+        modelsView += modelView(model);
 
+        if (!(i % 3) ) {
+            generationsArray.push(collectGeneration(model));
+            modelsView = '';
+            debugger;
+        }
+
+    }
+
+    function collectGeneration(model) {
+        return `<div class="generation">${model.generation}
+            ${modelsView}
+          </div>`
+    };
+
+    return generationsArray;
+}
+
+function modelView(model) {
+    return `<div class="${model.className}">
+            <img class="picture" src="${model.img}">
+            <div class="bodywork">${model.name}</div>
+           </div>`
+}
+
+initCarList(cars);
 
 class GenerationView {
     constructor(options) {
         this._el = options.element;
-        this.g1 = this._el.querySelector('.fst');
-        this.g2 = this._el.querySelector('.snd');
-        this.g3 = this._el.querySelector('.trd');
-        this.g4 = this._el.querySelector('.fth');
         this.btn = options.btn;
+        this.array = generationsArray;
 
         this.btn.addEventListener('click', this.sorting.bind(this));
-        }
+    }
 
     sorting() {
         let type = event.target.getAttribute('data-type');
 
         if (type === 'new') {
-            let new_g = this.g4.innerHTML + this.g3.innerHTML + this.g2.innerHTML + this.g1.innerHTML;
-            this._el.innerHTML = new_g;
+            generationsArray = [];
+            initCarList(cars);
         }
 
         if (type === 'old') {
-            let old_g = this.g1.innerHTML + this.g2.innerHTML + this.g3.innerHTML + this.g4.innerHTML;
-            this._el.innerHTML = old_g;
+            generationsArray.reverse();
+            container.innerHTML = generationsArray;
+            generationsArray = [];
         }
 
     }
 }
 
-let generation = new GenerationView({
+ let generation = new GenerationView({
     element: document.getElementById('all_generations'),
     btn: document.querySelector('.sorting'),
 
 });
-
-
-
-
